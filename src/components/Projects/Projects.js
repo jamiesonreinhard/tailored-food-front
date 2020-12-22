400ca53b12bfcf2f90f3f935719414c9f649c0c9
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2020-12-22 16:31:40 -0700

    can now toggle between projects on Projects page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,4 +1,4 @@
-import react from 'react';
+import React, { useState } from 'react';
 import Project from './Project';
 import {projects} from '../../data/projects';
 import { Container } from 'react-bootstrap';
@@ -7,20 +7,32 @@ const Projects = () => {
 
   const projectData = projects;
   console.log(projectData);
+  const [active, setActive] = useState(projectData[0]);
 
-  const projectDeck = projectData.map((project, index) => {
+  const projectDeck = projectData.map((project) => {
     return(
-      <Project key={index} project={project} />
+      <div>
+        <span className={active === project ? "project-name-active" : "project-name"} onClick={() => setActive(project)} key={project.name} active={active === project}>{project.name}</span><br />
+      </div>
+      
     )
   })
   
   return(
     <div className="projects">
       <Container>
-        <h2 className="projects-heading mb-3">Our Projects</h2>
-        <div className="project-grid">
-          {projectDeck}
+      <h2 className="projects-heading mb-5">Our Projects</h2>
+        <div className="row">
+          <div className="col-lg-4" style={{textAlign: "right"}}>
+          
+            {projectDeck}
+          </div> 
+          <div className="col-lg-8">
+            <Project project={active} />
+          </div>
         </div>
+        
+        
       </Container>
     </div>
   )
