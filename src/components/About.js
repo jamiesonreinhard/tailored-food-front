a3587cf154d88e9ccd55e5c877f5a23ffe25d173
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2021-01-05 17:34:44 -0700

    added back mapbox gl line to About

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 import ReactMapGl, { Marker, Popup } from 'react-map-gl';
 import {projects} from '../data/projects';
 // eslint-disable-next-line import/no-webpack-loader-syntax
-// mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
+mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
 
 const About = () => {
   const [viewport, setViewport] = useState({
