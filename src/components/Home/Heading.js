3324b84725d532521e0d25972e125d0fedb50db8
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2021-02-23 08:28:52 -0700

    added a slider to the header background image

---
+++ src/components/Home/Heading.js
@@ -0,0 +1,19 @@
+
+
+const Heading = () => {
+  return(
+    <div className="home-heading">
+        <div className="load">
+        </div>
+        <div className="home-heading-box">
+          <div className="principal">
+            <small className="home-secondary-title"><strong>WE BUILD FOOD SYSTEMS THAT ARE</strong></small>
+            <h1 className="home-title mb-4">Nutritious, Delicious, Affordable & Local</h1>
+            <h5 className="home-tertiary-title">We aim to end hunger by 2030 through local nutrition entrepreneurship.</h5>
+          </div>
+        </div> 
+      </div>
+  )
+}
+
+export default Heading;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,23 +3,14 @@ import BgVideo from '../../components/BgVideo';
 import TailoredModel from './TailoredModel';
 import Partners from './Partners';
 import HomeFooter from './HomeFooter';
+import Heading from './Heading';
 
 
 const Home = () => {
   
   return(
     <div className="home">
-        {/* <BgVideo /> */}
-      <div className="home-heading">
-        
-        <div className="home-heading-box">
-          <small className="home-secondary-title"><strong>WE BUILD FOOD SYSTEMS THAT ARE</strong></small>
-          <h1 className="home-title mb-4">Nutritious, Delicious, Affordable & Local</h1>
-          <h5 className="home-tertiary-title">We aim to end hunger by 2030 through local nutrition entrepreneurship.</h5>
-        </div>
-        
-      </div>
-
+      <Heading />
       <div className="container">
         <TailoredModel />
         <Partners />
