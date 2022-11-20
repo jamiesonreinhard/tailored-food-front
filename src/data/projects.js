93c9fe4520f4638fb4685c2435a969e6d5a4b82f
Jamieson Reinhard <jamieson@zoee.com>
2022-11-20 09:14:50 -0600

    updated team, about, and projects pages

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,21 +1,12 @@
 export const projects = [
   {
-    "location": "Mozambique",
-    "coordinates": [18.6657, 35.5296],
-    "product": "Peanut Butter",
-    "stage": "Development",
-    "description": "Launching the first low-cost nutritious peanut butter to ever be available in Mozambique, a country where peanut butter is wildly popular but is currently a luxury item. Sourced and ordered manufacturing equipment, completed market research, and currently completely facility build out to meet food safety standards before beginning peanut butter production in rural Mozambique.",
-    "partner_name": "Mozambique School Lunch Initiative",
-    "image": "mozambiqueschoollunch.png"
-  },
-  {
-    "location": "Brazil",
-    "coordinates": [-14.2350, -51.9253],
-    "product": "Plant-based ground beef",
-    "stage": "Launching",
-    "description": "Launching a soy-based ground ‘beef’ product targeting consumers in poverty. Market trials completed, female direct sales agents hired, launching in the favelas of Sao Paulo Fall 2020.",
-    "partner_name": "mr.veggy",
-    "image": "mrveggy.png"
+    "location": "Liberia",
+    "coordinates": [6.4281, -9.4295],
+    "product": "Cassava-based Fortified Porridges flavored with Sesame, Coconut, and Wild Palm",
+    "stage": "Operational",
+    "description": "In partnership with Kawadah Farms - Local Grassroots Business built, cassava-based flavored porridge line launched, over 1,000,000 meals purchased, licensing model operational for 5 years, currently adapting distribution strategy while scaling up through a partnership with the US Department of Agriculture's McGovern-Dole School Feeding Program.",
+    "partner_name": "Kawadah Farms",
+    "image": "kawadah.png"
   },
   {
     "location": "Congo",
@@ -27,21 +18,39 @@ export const projects = [
     "image": "WFP.jpg"
   },
   {
-    "location": "Madagascar",
-    "coordinates": [-18.7669, 46.8691],
-    "product": "Cassava and breadfruit-based snacks",
+    "location": "Mozambique",
+    "coordinates": [-18.6657, 35.5296],
+    "product": "Peanut Butter",
     "stage": "Development",
-    "description": "Consultant hired by UN World Food Programme, completing market research, product launch, and business growth work on cassava and breadfruit value chains in southern Madagascar.",
-    "partner_name": "UN World Food Programme Country Office",
-    "image": "WFP.jpg"
+    "description": "In partnership with Mozambique School Lunch Initiative - Launching the first low-cost nutritious peanut butter to ever be available in Mozambique, a country where peanut butter is wildly popular but is currently a luxury item. Sourced and ordered manufacturing equipment, completed market research, and ran five production trials before beginning low-cost peanut butter sales to families struggling with malnutrition in Mozambique.",
+    "partner_name": "Mozambique School Lunch Initiative",
+    "image": "mozambiqueschoollunch.png"
   },
   {
-    "location": "Liberia",
-    "coordinates": [6.4281, 9.4295],
-    "product": "Cassava-based Fortified Porridges flavored with Sesame, Coconut, and Wild Palm",
-    "stage": "Operational",
-    "description": "Local Grassroots Business built, cassava-based flavored porridge line launched, over 300,000 meals purchased, licensing model operational for 2.5 years, currently adapting distribution strategy and expanding and training sales team.",
-    "partner_name": "Kawadah Farms",
-    "image": "kawadah.png"
+    "location": "Cameroon",
+    "coordinates": [7.3697, 12.3547],
+    "product": "Plant-based ground beef",
+    "stage": "Launching",
+    "description": "In partnership with local social enterprise Kayvey Nutri Foods - Revamping Kayvey's nutritious porridge product line, improving cost, packaging, and distribution. The goal is to ensure Kayvey's line of nutritious, affordable porridge products are available to families in all regions of Cameroon.",
+    "partner_name": "Kayvey Nutri Foods",
+    "image": "kayvey.png"
+  },
+  {
+    "location": "Ethiopia",
+    "coordinates": [9.1450, 40.4897],
+    "product": "Plant-based ground beef",
+    "stage": "Launching",
+    "description": "In partnership with local social enterprise BeNu - reformulating BeNu's protein-rich, locally-produced full-meal biscuit products, improving the nutrition profile, the flavor, the product structure, while expanding the business in both the food aid and private markets, targeting people suffering from malnutrition.",
+    "partner_name": "BeNu",
+    "image": "benu.png"
+  },
+  {
+    "location": "Brazil",
+    "coordinates": [-14.2350, -51.9253],
+    "product": "Plant-based ground beef",
+    "stage": "Launching",
+    "description": "In partnership with mr.veggy - Launching a soy-based ground ‘beef’ product targeting consumers in poverty. Market trials completed, female direct sales agents hired, launched in the favelas of Sao Paulo.",
+    "partner_name": "mr.veggy",
+    "image": "mrveggy.png"
   }
 ]
--- src/data/team.js
+++ src/data/team.js
@@ -6,46 +6,10 @@ export const team = [
     "bio": "Taylor Quinn is a social entrepreneur dedicated to building nutritious low-cost food systems. He moved to Liberia during the Ebola outbreak, and used his background as a food anthropologist to build a Liberian-owned business that produces nutritious low-cost delicious food (flavored cassava-based porridges) for consumers in extreme poverty. He has since taken this model to work in 10 countries, from Brazil to Congo, while also scaling up this approach at the UN World Food Programme."
   },
   {
-    "name": "Emma Johnson",
-    "role": "Marketing",
-    "image": "Emma.png",
-    "bio": "Born in Ireland and raised in Canada, Emma has grown a passion for the dynamic world of sustainable development. Now based in London, Emma works in media and marketing with a specialization in helping companies meet their environmental, social, and governance requirements."
-  },
-  {
     "name": "Jamieson Reinhard",
     "role": "Software Engineer",
     "image": "Jamieson.png",
     "bio": "Jamieson is a passionate software engineer from Colorado. Jamieson found a knack for web development after searching for a career where he could be creative and solve important problems. He is freelancing on many non-profit projects, including this mission with Tailored Food. When he isn’t building the web, Jamieson can be found travelling to wherever he can ski, hike, golf, eat, or experience some culture."
-  },
-  {
-    "name": "Martin Agubata",
-    "role": "Web Designer",
-    "image": "Martin.png",
-    "bio": "Martin is a Graphic designer/illustrator and Product designer with more than 6 years of experience in design. He has designed for companies like Dash Cryptocurrency and a lot of companies in the tech space to name a few."
-  },
-  // {
-  //   "name": "Sagar Jha",
-  //   "role": "Marketing",
-  //   "image": "Sagar.png",
-  //   "bio": ""
-  // },
-  {
-    "name": "Medha Gupta",
-    "role": "Marketing",
-    "image": "Medha.png",
-    "bio": "Medha is a Nutritionist who wants to make a world a better place. She is a business strategiest with a drive to create a sustainable food experience and make healthy nutritious food accessible to all. Based in India workng as Nutrition consultant, scientific communication strategistst and collaborating with food companies globally that share a similar vision."
-  },
-  {
-    "name": "Laura Hackl",
-    "role": "Marketing",
-    "image": "laura.png",
-    "bio": "Laura is a pharmacist and nutrition scientist from Austria. Her interests are in public health and maternal and child nutrition – often in the context of micronutrient supply through fortified foods and food safety. She loves great conversations, knowing what motivates people around her, boardgames, baking, traveling, and time spent with her family, friends and her cats Mochi + Mitzi. One of Laura's motivations is helping to end malnutrition and improving the livelihoods of vulnerable populations."
-  },
-  {
-    "name": "Sandra Wagner",
-    "role": "Marketing",
-    "image": "Sandra.png",
-    "bio": "Food Scientist with 18+ years of experience in the Food and Ingredient industries. Business Development, Marketing and Global Product Management experience. Master of Science in Law from Northwestern University (Law/Business and Innovation). I am an active yoga practitioner and advocate for good nutrition and sustainability."
   },
   {
     "name": "Tanya Mayo-Bruinsma",
