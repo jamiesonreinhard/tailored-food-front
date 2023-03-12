a3cbbd931f14daba2abf722b612309b0d9009354
Jamieson Reinhard <jamieson@zoee.com>
2023-03-12 10:58:52 -0600

    add dara to team

--- src/data/team.js
+++ src/data/team.js
@@ -6,16 +6,16 @@ export const team = [
     "bio": "Taylor Quinn is a social entrepreneur dedicated to building nutritious low-cost food systems. He moved to Liberia during the Ebola outbreak, and used his background as a food anthropologist to build a Liberian-owned business that produces nutritious low-cost delicious food (flavored cassava-based porridges) for consumers in extreme poverty. He has since taken this model to work in 10 countries, from Brazil to Congo, while also scaling up this approach at the UN World Food Programme."
   },
   {
-    "name": "Jamieson Reinhard",
-    "role": "Software Engineer",
-    "image": "Jamieson.png",
-    "bio": "Jamieson is a passionate software engineer from Colorado. Jamieson found a knack for web development after searching for a career where he could be creative and solve important problems. He is freelancing on many non-profit projects, including this mission with Tailored Food. When he isn’t building the web, Jamieson can be found travelling to wherever he can ski, hike, golf, eat, or experience some culture."
-  },
-  {
     "name": "Tanya Mayo-Bruinsma",
-    "role": "Marketing",
+    "role": "Project Director",
     "image": "tanya.png",
     "bio": "As a mother of two, Tanya knows how to put the fun into food and juggle between what the eyes savor and the body needs. When she is not busy turning her kitchen into a catering business, she explores the mountains around her French alpine home by paraglider, on skis or by foot."
+  },
+  {
+    "name": "Dara Karakolis",
+    "role": "Communications Manager",
+    "image": "dara.jpg",
+    "bio": "Dara Karakolis is an emerging sustainability specialist with experience in nutrition and food systems including health, advocacy, and corporate relations. In addition to effective communication and project/event management, she coordinated multidisciplinary stakeholders (youth groups, professional bodies, private sector, NGOs) and a global campaign called Act4Food Act4Change. When she’s not working towards food systems transformation you’ll find her traveling the world, cooking or hiking a mountain!"
   },
   {
     "name": "Rachel D'armiento",
@@ -23,4 +23,10 @@ export const team = [
     "image": "rachel.png",
     "bio": "Rachel is an adventurous and ambitious project manager who enjoys spending time engaging deeply with communities. Rachel has a strong passion for sustainable global development and gender equality and she is pursuing her studies in International development and Social Anthropology as she works to build impact-driven organizations."
   },
+  {
+    "name": "Jamieson Reinhard",
+    "role": "Software Engineer",
+    "image": "Jamieson.png",
+    "bio": "Jamieson is a passionate software engineer from Colorado. Jamieson found a knack for web development after searching for a career where he could be creative and solve important problems. He is freelancing on many non-profit projects, including this mission with Tailored Food. When he isn’t building the web, Jamieson can be found travelling to wherever he can ski, hike, golf, eat, or experience some culture."
+  }
 ]
